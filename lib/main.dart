8111c40986eac5a6a50b872166a0e090e02cd0ba
Marcel Garus <marcel.garus@gmail.com>
2020-07-17 21:05:32 +0200

    Generate random objects and use tape to encode them and write chunks

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,85 @@
 import 'dart:io';
+import 'dart:math';
+
+import 'package:tape/tape.dart';
 
 import 'chunky/chunk.dart';
 import 'chunky/chunky.dart';
 
 void main() async {
+  Tape.registerDartCoreAdapters();
+
   print('Hello world.');
 
   final chunky = await Chunky.named('sample');
-  print('Chunk file path is ${chunky.chunkFile.path}');
-  final firstChunk = await chunky.read(0);
-
-  // Writes on chunks only change the data in memory. To actually write them to
-  // disk, `chunky.write` has to be used inside a transaction.
-  firstChunk.setUint8(24, 42);
-
-  await chunky.transaction((chunky) async {
-    await chunky.write(0, firstChunk);
+  var index = 0;
+  var bucket = BucketChunk();
+  chunky.transaction((chunky) {
+    chunky.write(0, bucket.chunk);
   });
 
-  final newChunk = Chunk.empty();
-  newChunk.setUint8(1, firstChunk.getUint8(2));
-
-  // Inside transactions, multiple chunks can be written to.
-  await chunky.transaction((chunky) async {
-    final chunkId = await chunky.add(newChunk);
-    firstChunk.setUint8(12, chunkId);
-    await Future.delayed(Duration(seconds: 2));
-    await chunky.write(0, firstChunk);
-  });
-
-  await chunky.transaction((chunky) async {
-    int id = chunky.add(Chunk.empty());
-    print('Reserved chunk and got id $id.');
-  });
+  while (true) {
+    var object = generateObject();
+    try {
+      bucket.add(object);
+    } catch (e) {
+      print('Bucket $index full (${bucket.objects.length}): ${bucket.chunk}');
+      bucket = BucketChunk()..add(object);
+      index++;
+      chunky.transaction((chunky) => chunky.write(index, bucket.chunk));
+    }
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
+
+class BucketChunk {
+  final chunk = Chunk.empty();
+  final objects = <Object, List<int>>{};
+
+  int get usedBytes =>
+      1 +
+      objects.values.fold(0, (sum, bytes) => sum + bytes.length) +
+      objects.length * 2;
+  int get freeBytes => chunkSize - usedBytes;
+
+  void add(Object obj) {
+    final bytes = tape.encode(obj);
+    if (objects.length >= 256 || bytes.length > freeBytes) {
+      throw "Doesn't fit.";
+    }
+    objects[obj] = bytes;
+    chunk.setUint8(0, objects.length);
+
+    var pointerCursor = 1;
+    var objectCursor = chunkSize;
+
+    for (final bytes in objects.values) {
+      objectCursor -= bytes.length;
+      chunk.setUint16(pointerCursor, objectCursor);
+      chunk.setBytes(objectCursor, bytes);
+      pointerCursor += 2;
+    }
+  }
+}
+
+dynamic generateObject() {
+  final generators = <dynamic Function()>[
+    generateDuration,
+    generateDouble,
+    generateString,
+    generateInt,
+  ];
+  return generators[Random().nextInt(generators.length)]();
+}
+
+Duration generateDuration() =>
+    Duration(microseconds: Random().nextInt(1000000));
+
+double generateDouble() => Random().nextDouble();
+
+String generateString() {
+  const fruits = ['apple', 'banana', 'kiwi', 'orange', 'passion fruit'];
+  return fruits[Random().nextInt(fruits.length)];
+}
+
+int generateInt() => Random().nextInt(10);
