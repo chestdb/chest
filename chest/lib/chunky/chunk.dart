12da005390052a4111c867c8064cb1ee4db75026
Marcel Garus <marcel.garus@gmail.com>
2020-08-04 22:21:17 +0200

    Clean up Chunk

--- chest/lib/chunky/chunk.dart
+++ chest/lib/chunky/chunk.dart
@@ -6,35 +6,36 @@ import 'package:convert/convert.dart';
 const chunkSize = 64;
 
 class Chunk {
-  Chunk._(this._data, this._list)
-      : assert(_data.lengthInBytes == chunkSize),
-        assert(_list.length == chunkSize);
   Chunk() : this.fromByteData(ByteData(chunkSize));
-  Chunk.fromByteData(this._data) : _list = Uint8List.view(_data.buffer);
-  Chunk.fromUint8List(this._list) : _data = ByteData.view(_list.buffer);
+  Chunk.fromByteData(this.byteData)
+      : assert(byteData.lengthInBytes == chunkSize),
+        bytes = Uint8List.view(byteData.buffer);
+  Chunk.fromUint8List(this.bytes)
+      : assert(bytes.length == chunkSize),
+        byteData = ByteData.view(bytes.buffer);
+  factory Chunk.copyOf(Chunk other) => Chunk()..copyFrom(other);
 
-  final ByteData _data;
-  final Uint8List _list;
+  final ByteData byteData;
+  final Uint8List bytes;
+  ByteBuffer get buffer => bytes.buffer;
 
-  Uint8List get bytes => _list;
-  ByteBuffer get buffer => _list.buffer;
+  int getUint8(int offset) => byteData.getUint8(offset);
+  void setUint8(int offset, int value) => byteData.setUint8(offset, value);
+  int getUint16(int offset) => byteData.getUint16(offset);
+  void setUint16(int offset, int value) => byteData.setUint16(offset, value);
+  int getInt64(int offset) => byteData.getInt64(offset);
+  void setInt64(int offset, int value) => byteData.setInt64(offset, value);
 
-  void setUint8(int offset, int value) => _data.setUint8(offset, value);
-  int getUint8(int offset) => _data.getUint8(offset);
-  void setUint16(int offset, int value) => _data.setUint16(offset, value);
-  int getUint16(int offset) => _data.getUint16(offset);
-  void setInt64(int offset, int value) => _data.setInt64(offset, value);
-  int getInt64(int offset) => _data.getInt64(offset);
-
+  Uint8List getBytes(int offset, int length) =>
+      Uint8List.view(buffer, offset, length);
   void setBytes(int offset, List<int> bytes) {
     for (var i = 0; i < bytes.length; i++) {
       setUint8(offset + i, bytes[i]);
     }
   }
 
-  List<int> getBytes(int offset, int length) {
-    return <int>[for (var i = 0; i < length; i++) getUint8(offset + i)];
-  }
+  void copyTo(Chunk other) => other.setBytes(0, bytes);
+  void copyFrom(Chunk other) => other.copyTo(this);
 
-  String toString() => hex.encode(_list);
+  String toString() => hex.encode(bytes);
 }
