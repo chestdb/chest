29b168cd5aa590703027d299365cb6acc27e5f7f
Marcel Garus <marcel.garus@gmail.com>
2020-08-04 22:22:46 +0200

    Add ChunkManager

--- chest/lib/vm_chest/chunks/free_chunk.dart
+++ chest/lib/vm_chest/chunks/free_chunk.dart
@@ -21,3 +21,38 @@ class FreeChunk extends ChunkWrapper {
   int get nextChunkId => chunk.getChunkId(1);
   set nextChunkId(int id) => chunk.setChunkId(1, id);
 }
+
+class ChunkManager {
+  ChunkManager(this.chunky);
+
+  final ChunkyTransaction chunky;
+
+  Chunk reserveChunk() {
+    final main = MainChunk(chunky.read(0));
+    final chunk = Chunk();
+    final freeChunkIndex = main.firstFreeChunk;
+    if (freeChunkIndex == 0) {
+      chunky.add(chunk);
+    } else {
+      chunky.readInto(freeChunkIndex, chunk);
+      final nextFreeChunkIndex = FreeChunk(chunk).nextChunkId;
+      main.firstFreeChunk = nextFreeChunkIndex;
+      chunky.write(0, main);
+    }
+    return chunk;
+  }
+
+  void freeChunk(int index) {
+    final main = MainChunk(chunky.read(0));
+    final freeChunkIndex = main.firstFreeChunk;
+    if (freeChunkIndex == 0) {
+      main.firstFreeChunk = index;
+      chunky.write(0, main);
+    } else {
+      final chunk = FreeChunk(chunky.read(index));
+      chunk.nextChunkId = freeChunkIndex;
+      main.firstFreeChunk = index;
+      chunky..write(index, chunk)..write(0, main);
+    }
+  }
+}
