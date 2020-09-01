f6e8d05eefd3ec7dec8c3f027691d45b94056d80
Marcel Garus <marcel.garus@gmail.com>
2020-09-01 19:49:32 +0200

    Define api classes

--- chest/lib/chest.dart
+++ chest/lib/chest.dart
@@ -1,4 +1,50 @@
+part 'query.dart';
+
+/// A representation of a single database.
+///
+/// # Usage
+///
+/// ```
+/// final chest = Chest('my_database');
+/// final users = chest.box<String, User>('users');
+/// final me = users.doc('marcelgarus');
+/// print(await me.get());
+/// final myTodos = me.box('todos');
+/// final alsoMyTodos = chest.box('users').doc('marcelgarus').box('todos');
+/// ```
+///
+/// # Limitations
+///
+/// - Queries cannot cover multiple [Chest]s.
+/// - On the VM, each [Chest] is stored in one file. Your OS may limit the
+///   number of simultaneously opened files (typically a huge number).
 abstract class Chest {
-  Chest(String name);
-  Future<void> put(List<int> key, Object value);
+  factory Chest(String name) {
+    // TODO: Conditionally use VmChest or WebChest.
+    return VmChest(name);
+  }
+
+  Box<K, V> box<K, V>(String name);
+}
+
+/// A part of the [Chest] that contains some [Doc]s.
+abstract class Box<K, V> {
+  Doc<V> doc(K key);
+  QueryResult<V> rawQuery(Query<IndexedClass<V>> query) {
+    print('Executing raw query $query.');
+    return null;
+  }
+}
+
+/// A [Doc]ument holding some data.
+///
+/// Can have [Box]es as children.
+abstract class Doc<V> {
+  Box<K, W> box<K, W>(String name);
+
+  Future<bool> exists();
+  Future<void> set(V value);
+  Future<V> get();
+  Future<void> remove();
+  Stream<V> watch();
 }
