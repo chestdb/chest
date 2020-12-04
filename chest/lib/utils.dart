abea7a53488ff4245a51d76ff624ac844320c635
Marcel Garus <marcel.garus@gmail.com>
2020-12-04 20:10:55 +0100

    Add utils

--- chest/lib/utils.dart
+++ chest/lib/utils.dart
@@ -1,5 +1,17 @@
+extension IterableX<T> on Iterable<T> {
+  T? get firstOrNull {
+    return isEmpty ? null : cast<T?>().firstWhere((_) => true, orElse: null);
+  }
+}
+
 extension WhereKeyValue<K, V> on Iterable<MapEntry<K, V>> {
-  Iterable<MapEntry<K, V>> whereKeyValue(
-          bool Function(K key, V value) callback) =>
-      where((entry) => callback(entry.key, entry.value));
+  Iterable<MapEntry<K, V>> whereKeyValue(bool Function(K key, V value) check) {
+    return where((entry) => check(entry.key, entry.value));
+  }
+
+  Iterable<T> mapKeyValue<T>(T Function(K key, V value) mapper) {
+    return map((entry) => mapper(entry.key, entry.value));
+  }
+
+  Map<K, V> toMap() => Map.fromEntries(this);
 }
