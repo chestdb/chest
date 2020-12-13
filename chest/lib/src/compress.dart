10f4898cd7a66b89adbce460fe18e6f2e32e876a
Marcel Garus <marcel.garus@gmail.com>
2020-12-13 23:06:19 +0100

    Review compression

--- chest/lib/src/compress.dart
+++ chest/lib/src/compress.dart
@@ -1,8 +1,33 @@
 import 'dart:typed_data';
 
-extension Compress on List<int> {
+/// Compresses the bytes with a tape-specific compression.
+///
+/// Tape encodings typically have long runs of zeros in them because type codes,
+/// lengths, and pointers don't usually fill the whole uint64 assigned to them.
+/// So this compression turns multiple zeros into a zero followed by the number
+/// of zeros follwed.
+///
+/// A few examples:
+///
+/// Five zeros become one zero followed by a four indicating four more zeroes:
+/// original:   1 2 3 0 0 0 0 0 4 5 6
+/// compressed: 1 2 3 0 4 4 5 6
+///
+/// A single zero becomes a zero followed by a zero indicating no more zero:
+/// original:   1 0 1 0 1 0 1
+/// compressed: 1 0 0 1 0 0 1 0 0 1
+///
+/// A good property of this compression is that it's simple, fast, and can be
+/// applied on-the-fly. As seen above, maliciously designed values can cause it
+/// to actually increase the needed space. But that happens rarely with values
+/// encoded in the real world, so compressing is still a good decision in 99 %
+/// of the cases.
+/// For cases where space really matters, you should use general compression
+/// algorithms anyways – those will handle runs of 1s and 0s like in the second
+/// example gracefully.
+extension Compression on List<int> {
   Uint8List compress() {
-    // TODO: Make this more efficient.
+    // TODO: Make this more efficient and safe (the encoding might also be longer).
     final out = ByteData(length);
     var cursor = 0;
 
@@ -28,9 +53,7 @@ extension Compress on List<int> {
 
     return Uint8List.view(out.buffer, 0, cursor);
   }
-}
 
-extension Decompress on List<int> {
   Uint8List decompress() {
     final out = BytesBuilder();
 
