9371743cccbfb0443ec0cfa9f733c2f4d4288e35
Marcel Garus <marcel.garus@gmail.com>
2021-01-02 19:27:15 +0100

    Also always register content tapers

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -1,6 +1,23 @@
 // Dummy annotation classes.
 
+import 'content.dart';
 import 'tapers.dart';
+
+/// Main API for the tape part.
+const tape = TapeApi();
+
+class TapeApi {
+  const TapeApi();
+
+  void register(Map<int, Taper<dynamic>> typeCodesToTapers) {
+    registry.register({
+      ...tapersForContent,
+      ...typeCodesToTapers,
+    });
+  }
+
+  bool get isInitialized => registry.hasTapers;
+}
 
 class TapeKey {
   const TapeKey(String key);
@@ -20,17 +37,4 @@ const tapers = TapersForPackageApi();
 
 class TapersForPackageApi {
   const TapersForPackageApi();
-}
-
-/// Main API for the tape part.
-const tape = TapeApi();
-
-class TapeApi {
-  const TapeApi();
-
-  void register(Map<int, Taper<dynamic>> typeCodesToTapers) {
-    registry.register(typeCodesToTapers);
-  }
-
-  bool get isInitialized => registry.hasTapers;
 }
